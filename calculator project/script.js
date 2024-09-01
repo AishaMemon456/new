1c38794d26774c1c5608e1dfc583c8004ea54ca5
Aishamemon456 <memonaisha639gmail.com>
2024-08-31 17:58:16 -0700

    change

---
+++ calculator project/script.js
@@ -0,0 +1,21 @@
+function appendToDisplay(value) {
+    let display = document.getElementById('display');
+    if (display.innerText === '0') {
+        display.innerText = value;
+    } else {
+        display.innerText += value;
+    }
+}
+
+function clearDisplay() {
+    document.getElementById('display').innerText = '0';
+}
+
+function calculateResult() {
+    let display = document.getElementById('display');
+    try {
+        display.innerText = eval(display.innerText);
+    } catch (e) {
+        display.innerText = 'Error';
+    }
+}
